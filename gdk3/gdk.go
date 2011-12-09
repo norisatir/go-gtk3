6878108dee0e1759c093587b588da2b42f1b32e0
Matej Knific <norisatir@gmail.com>
2011-12-09 21:41:06 +0100

    Added ThreadsInit, ThreadsEnter, ThreadsLeave.

--- gdk3/gdk.go
+++ gdk3/gdk.go
@@ -3,7 +3,23 @@ package gdk3
 /*
 #include <gdk/gdk.h>
 
+static void _gdk_threads_init() {
+	gdk_threads_init();
+}
+
 */
 // #cgo pkg-config: gdk-3.0
 import "C"
 import _ "github.com/norisatir/go-gtk3/gobject"
+
+func ThreadsInit() {
+	C._gdk_threads_init()
+}
+
+func ThreadsEnter() {
+	C.gdk_threads_enter()
+}
+
+func ThreadsLeave() {
+	C.gdk_threads_leave()
+}
