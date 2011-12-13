0fb9092269b7382280ce71909e3de772c5186069
Matej Knific <norisatir@gmail.com>
2011-12-13 15:55:20 +0100

    Added GList to glib binding.

--- glib/glib.go
+++ glib/glib.go
@@ -132,6 +132,106 @@ func (self *GSList) Foreach(f interface{}, data ...interface{}) {
 // End GSList
 ////////////////////////////// }}}
 
+// GList {{{
+//////////////////////////////
+
+// GList type
+// GList type which holds C GList.
+// If GC_Free is true, then GC will call g_list_free on object
+// IF GC_FreeFull it true then GC will call g_list_free_full
+// even if GC_Free is true/false (it is ignored).
+type GList struct {
+	object         *C.GList
+	GC_Free        bool
+	GC_FreeFull    bool
+	ConversionFunc ConverterFunc
+	DestroyFunc    ListClosure
+}
+
+// NewGList returns new GList with first element initialized.
+// GC_Free and GC_FreeFull will hold default bool values.
+func NewGList() *GList {
+	gl := &GList{}
+	gl.object = C.g_list_alloc()
+	gl.ConversionFunc = nil
+	gl.DestroyFunc = nil
+
+	return gl
+}
+
+func NewGListFromNative(list unsafe.Pointer) *GList {
+	gl := &GList{}
+	gl.object = (*C.GList)(list)
+	gl.ConversionFunc = nil
+	gl.DestroyFunc = nil
+
+	return gl
+}
+
+// GList finalizer
+func GListFinalizer(gl *GList) {
+	runtime.SetFinalizer(gl, func(gl *GSList) {
+		if gl.GC_FreeFull {
+			gl.FreeFull()
+			return
+		}
+
+		if gl.GC_Free {
+			gl.Free()
+		}
+	})
+}
+
+func (self *GList) Free() {
+	C.g_list_free(self.object)
+}
+
+func (self *GList) FreeFull() {
+	if self.DestroyFunc == nil {
+		return
+	}
+	var numElements int = int(self.Length())
+
+	for i := 0; i < numElements; i++ {
+		el := C.g_list_nth_data(self.object, C.guint(i))
+		self.DestroyFunc(unsafe.Pointer(el))
+	}
+	C.g_list_free(self.object)
+}
+
+func (self *GList) NthData(n uint) interface{} {
+	data := C.g_list_nth_data(self.object, C.guint(n))
+
+	if data == nil {
+		return nil
+	}
+
+	if self.ConversionFunc != nil {
+		return self.ConversionFunc(unsafe.Pointer(data))
+	}
+	return data
+}
+
+func (self *GList) Length() uint {
+	return uint(C.g_list_length(self.object))
+}
+
+func (self *GList) Foreach(f interface{}, data ...interface{}) {
+	// Create id and closure
+	cl, _ := gobject.CreateCustomClosure(f, data...)
+	listLength := int(self.Length())
+
+	for i := 0; i < listLength; i++ {
+		data := self.NthData(uint(i))
+		if data != nil {
+			cl([]interface{}{data})
+		}
+	}
+}
+//////////////////////////////
+// End GList
+////////////////////////////// }}}
+
 func GTimeoutAddFull(priority int, interval uint, callback interface{}, data ...interface{}) {
 	cl, id := gobject.CreateCustomClosure(callback, data...)
 	_closures[id] = cl
