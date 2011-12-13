44be58cb28418ffd4251aa28db20b7fb1fda1c36
Matej Knific <norisatir@gmail.com>
2011-12-13 11:09:09 +0100

    Added GTimeoutFull to glib binding.

--- glib/glib.go
+++ glib/glib.go
@@ -2,6 +2,16 @@ package glib
 
 /*
 #include <glib-object.h>
+#include <stdlib.h>
+
+extern gboolean _g_source_func(gpointer user_data);
+extern void _g_destroy_notify(gpointer user_data);
+
+static void _g_timeout_add_full(gint priority, guint interval, gint64 id) {
+	gint64* uid = (gint64*)malloc(sizeof(gint64));
+	*uid = id;
+	g_timeout_add_full(priority, interval, _g_source_func, (gpointer)uid, _g_destroy_notify);
+}
 
 */
 // #cgo pkg-config: gobject-2.0
@@ -107,7 +117,7 @@ func (self *GSList) Foreach(f interface{}, data ...interface{}) {
 	for i := 0; i < listLength; i++ {
 		data := self.NthData(uint(i))
 		if data != nil {
-			cl([]interface{}{self.NthData(uint(i))})
+			cl([]interface{}{data})
 		}
 	}
 }
@@ -116,11 +126,52 @@ func (self *GSList) Foreach(f interface{}, data ...interface{}) {
 // End GSList
 ////////////////////////////// }}}
 
-func GTimeoutAddFull(priority int, interval uint, callback interface{}) {
-
+func GTimeoutAddFull(priority int, interval uint, callback interface{}, data ...interface{}) {
+	cl, id := gobject.CreateCustomClosure(callback, data...)
+	_closures[id] = cl
+	C._g_timeout_add_full(C.gint(priority), C.guint(interval), C.gint64(id))
 }
+
 // Exported functions
+//export _g_source_func
+func _g_source_func(user_data unsafe.Pointer) C.gboolean {
+	id := *((*C.gint64)(user_data))
+	var res bool
+
+	if f, ok := _closures[int64(id)]; ok {
+		res = f([]interface{}{})
+	}
+	b := gobject.GBool(res)
+	defer b.Free()
+	return *((*C.gboolean)(b.GetPtr()))
+}
+
+//export _g_destroy_notify
+func _g_destroy_notify(user_data unsafe.Pointer) {
+	id := *((*C.gint64)(user_data))
+	if _, ok := _closures[int64(id)]; ok {
+		delete(_closures, int64(id))
+	}
+	C.free(user_data)
+}
+
+var GPriority gPriority
+
+type gPriority struct {
+	HIGH int
+	DEFAULT int
+	HIGH_IDLE int
+	DEFAULT_IDLE int
+	LOW int
+}
 
 func init() {
 	_closures = make(map[int64]gobject.ClosureFunc)
+
+	// Initialize GPriority struct
+	GPriority.HIGH = -100
+	GPriority.DEFAULT = 0
+	GPriority.HIGH_IDLE = 100
+	GPriority.DEFAULT_IDLE = 200
+	GPriority.LOW = 300
 }
