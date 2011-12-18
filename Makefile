30a9e5572b6bfbeb4cdb78c759d98825b6178bce
Matej Knific <norisatir@gmail.com>
2011-12-18 18:57:51 +0100

    Started Pango binding.

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 include $(GOROOT)/src/Make.inc
 
-GC=${O}g -I../gobject/_obj -I ../glib/_obj -I../gdk3/_obj -I$(GOROOT)/pkg/$(GOOS)_$(GOARCH)
+GC=${O}g -I../gobject/_obj -I ../glib/_obj -I../gdk3/_obj -I../gdkpixbuf/_obj -I../pango/_obj -I$(GOROOT)/pkg/$(GOOS)_$(GOARCH)
 
 all:
 	cd gobject && gomake
 	cd glib && gomake
 	cd gdk3 && gomake
 	cd gdkpixbuf && gomake
+	cd pango && gomake
 	cd gtk3 && gomake
 
 install:
@@ -14,6 +15,7 @@ install:
 	cd glib && gomake install
 	cd gdk3 && gomake install
 	cd gdkpixbuf && gomake install
+	cd pango && gomake install
 	cd gtk3 && gomake install
 
 clean:
@@ -21,6 +23,7 @@ clean:
 	cd glib && gomake clean
 	cd gdk3 && gomake clean
 	cd gdkpixbuf && gomake clean
+	cd pango && gomake clean
 	cd gtk3 && gomake clean
 	cd demo_apps && gomake clean
 
@@ -29,6 +32,7 @@ fmt_all:
 	gofmt -w ./glib/*.go
 	gofmt -w ./gdk3/*.go
 	gofmt -w ./gdkpixbuf/*.go
+	gofmt -w ./pango/*.go
 	gofmt -w ./gtk3/*.go
 
 demo: install
---
+++ pango/pango.go
@@ -0,0 +1,90 @@
+package pango
+
+/*
+#include <pango/pango.h>
+*/
+// #cgo pkg-config: pango
+import "C"
+/*import "unsafe"
+import "runtime"
+import "github.com/norisatir/go-gtk3/gobject"
+*/
+
+// PangoWeight {{{
+
+var PangoWeight pangoWeight
+
+type pangoWeight struct {
+	THIN       int
+	ULTRALIGHT int
+	LIGHT      int
+	BOOK       int
+	NORMAL     int
+	MEDIUM     int
+	SEMIBOLD   int
+	BOLD       int
+	ULTRABOLD  int
+	HEAVY      int
+	ULTRAHEAVY int
+}
+// PangoWeight }}}
+
+// PangoStyle {{{
+
+var PangoStyle pangoStyle
+
+type pangoStyle struct {
+	NORMAL  int
+	OBLIQUE int
+	ITALIC  int
+}
+
+// End PangoStyle }}}
+
+// PangoScale {{{
+
+var PangoScale pangoScale
+
+type pangoScale struct {
+	Scale    int
+	XX_SMALL float64
+	X_SMALL  float64
+	SMALL    float64
+	MEDIUM   float64
+	LARGE    float64
+	X_LARGE  float64
+	XX_LARGE float64
+}
+// End PangoScale }}}
+
+// PANGO init funcs {{{
+func init() {
+
+	// Initialize PangoWeight
+	PangoWeight.THIN = 100
+	PangoWeight.ULTRALIGHT = 200
+	PangoWeight.LIGHT = 300
+	PangoWeight.BOOK = 380
+	PangoWeight.NORMAL = 400
+	PangoWeight.MEDIUM = 500
+	PangoWeight.SEMIBOLD = 600
+	PangoWeight.BOLD = 700
+	PangoWeight.ULTRABOLD = 800
+	PangoWeight.HEAVY = 900
+	PangoWeight.ULTRAHEAVY = 1000
+
+	// Initialize PangoStyle
+	PangoStyle.NORMAL = 0
+	PangoStyle.OBLIQUE = 1
+	PangoStyle.ITALIC = 2
+
+	// Initialize PangoScale
+	PangoScale.Scale = 1024
+	PangoScale.XX_SMALL = float64(0.5787037037037)
+	PangoScale.X_SMALL = float64(0.6444444444444)
+	PangoScale.SMALL = float64(0.8333333333333)
+	PangoScale.MEDIUM = float64(1.0)
+	PangoScale.LARGE = float64(1.2)
+	PangoScale.X_LARGE = float64(1.4399999999999)
+	PangoScale.XX_LARGE = float64(1.728)
+} // }}}
